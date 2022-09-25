25f1a8554d7cb0f9ca5d0c661489fff32f678f5e
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-09-25 21:48:41 +0000

    [Update]Bootstrap,Fontawesomeの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  root to: "homes#top"
   devise_for :users
+
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
