462859f394a00e14a9b138a60348395c0e3e222e
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-09-26 12:48:10 +0000

    [Add]アソシエーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :books, dependent: :destroy
+
   has_one_attached :profile_image
+
 
   def get_profile_image(width, height)
     unless profile_image.attached?
