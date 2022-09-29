31828f81476159a7f7a009a4b3476c26725cfeb7
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-09-29 07:59:15 +0000

    [Add]User info（未完成）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(current_user.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
   end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+
 end
--- db/migrate/20220925130728_create_books.rb
+++ db/migrate/20220925130728_create_books.rb
@@ -1,4 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
+
+  has_one_attached :image
+
   def change
     create_table :books do |t|
       t.string :title
