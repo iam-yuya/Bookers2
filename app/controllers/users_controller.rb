31e921a1621abda828648bd1552290ae875bbd44
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-10-04 09:56:18 +0000

    [Finish]users/showページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def destroy
