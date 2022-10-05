c574fc2fdb1da754cc232e0ec1b0c660ecc6c60b
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-10-05 07:31:47 +0000

    [Add]フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
     # @book（投稿データ）のuser_idを、current_user.id（今ログインしているユーザーのid）に
     # 指定することで投稿データに、ログイン中のユーザーのidを持たせている
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       render :index
@@ -31,6 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
   end
 
