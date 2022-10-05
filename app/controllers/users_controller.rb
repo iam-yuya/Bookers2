a755c23b2bedaa211193d0cc72bf3efaec96be8c
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-10-05 05:54:55 +0000

    [Finish]users/editページの作成、update機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,14 @@ class UsersController < ApplicationController
     @books = @user.books
   end
 
-  def destroy
+  def edit
+    @user = User.find(params[:id])
   end
 
-  def edit
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
